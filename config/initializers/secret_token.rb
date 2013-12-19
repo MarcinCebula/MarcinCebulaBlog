ab4cbb98bd7179185f117438b51c8c53495a7238
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2013-12-19 13:19:04 -0500

    Inital Project Commit   * Added empty working project   * Add gitignore with github defaults for rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarcinCebula::Application.config.secret_key_base = '63ae74cc9295a634ac942986b6254fba0eb9b43a5e14683c675796baa4bb5a076e67d302db63cb25d27bdefd9bd423474357b29baba628d92d4174acb34d890a'
